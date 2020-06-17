ea9d74cf9137685c04e92f268545640a7f787716
leonsnill <leonxnill@gmail.com>
2020-06-17 19:26:26 +0200

    Trend

--- learthengine/sandbox.py
+++ learthengine/sandbox.py
@@ -2,22 +2,22 @@ import ee
 ee.Initialize()
 
 kwargs = {
-    'sensor': 'L8',
-    'bands': ['LST'],  # ['B', 'G', 'R', 'NIR', 'SWIR1', 'SWIR2']
+    'sensor': 'LS',
+    'bands': ['NDVI'],  # ['B', 'G', 'R', 'NIR', 'SWIR1', 'SWIR2']
     'pixel_resolution': 30,
     'cloud_cover': 50,
     'masks': ['cloud', 'cshadow', 'snow'],
-    'roi': [10.6810,55.208, 18.4868,59.037],  # 38.4824, 8.7550, 39.0482, 9.2000 Addis
-    'score': 'STM',
+    'roi': [-154.721,70.1400, -152.3431,70.9540],  # 38.4824, 8.7550, 39.0482, 9.2000 Addis
+    'score': 'TS_slope',
     'reducer': ee.Reducer.median(),
-    'target_years': [2018],  # 1985, 1990, 1995, 2000, 2005, 2010, 2015, 2020
-    'surr_years': 2,
+    'target_years': [2000],  # 1985, 1990, 1995, 2000, 2005, 2010, 2015, 2020
+    'surr_years': 19,
     'target_doys': [182],  # [16, 46, 75, 105, 136, 166, 197, 228, 258, 289, 319, 350]
     'doy_range': 30,
-    'exclude_slc_off': True,
+    'exclude_slc_off': False,
     'export_option': 'Drive',
     'asset_path': "users/leonxnill/Addis/",
-    'export_name': 'LST_MED_SouthernSweden',
+    'export_name': 'NDVI_TS_ALASKA',
     'lst_threshold': 5,
     'wv_method': 'ERA5'
 }
